cf64c8a88b3ba1d346d93e58fcd51ee27504e8d2
Yongbb_13 <hvyongkim2@naver.com>
2024-02-22 18:45:47 +0900

    예외처리와 오류페이지 0222

---
+++ exccption/src/main/java/hello/exccption/ExccptionApplication.java
@@ -0,0 +1,13 @@
+package hello.exccption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExccptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExccptionApplication.class, args);
+    }
+
+}
---
+++ exccption/src/main/java/hello/exccption/WebConfig.java
@@ -0,0 +1,37 @@
+package hello.exccption;
+
+import hello.exccption.filter.LogFilter;
+import hello.exccption.interceptor.LogInterceptor;
+import jakarta.servlet.DispatcherType;
+import jakarta.servlet.Filter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "*.ico", "/error", "/error-page/**"); // 오류경로를 다빼버린다.
+    }
+
+//    @Bean
+    public FilterRegistrationBean logFilter(){
+
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<Filter>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+//        filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST);
+
+        return filterRegistrationBean;
+
+    }
+
+}
